3ddb41979309b799675e3a749c7974205d098c97
Geoffrey Brunet <geoffrey.brunet@icloud.com>
2021-12-02 23:13:56 +0100

    add comments for TODO

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 mod routes;
 mod structs;
 
-use actix_web::{web, App, HttpServer};
 use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpServer};
 use mobc::Pool;
 use mobc_redis::{redis, RedisConnectionManager};
+use routes::cache::{delete_cache, get_cache, post_cache};
 use routes::health_check::health_check;
-use routes::cache::{get_cache, post_cache};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -21,6 +21,7 @@ async fn main() -> std::io::Result<()> {
             .route("/health_check", web::get().to(health_check))
             .service(get_cache)
             .service(post_cache)
+            .service(delete_cache)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- src/routes/cache.rs
+++ src/routes/cache.rs
@@ -1,19 +1,21 @@
-use actix_web::{get, post, delete, web, HttpResponse, Responder};
-use mobc_redis::RedisConnectionManager;
-use mobc_redis::redis::AsyncCommands;
 use crate::structs::Info;
+use actix_web::{delete, get, post, web, HttpResponse, Responder};
+use mobc_redis::redis::AsyncCommands;
+use mobc_redis::RedisConnectionManager;
 type Pool = mobc::Pool<RedisConnectionManager>;
 
 #[get("/cache/{key}")]
 pub async fn get_cache(pool: web::Data<Pool>, info: web::Path<Info>) -> impl Responder {
     // TODO: return errors
     let mut conn = pool.get().await.unwrap();
+    //HttpResponse::error("Key don't exists")
     let s: String = conn.get(info.key.to_string()).await.unwrap();
     HttpResponse::Ok().body(s)
 }
 
 #[post("/cache/{key}")]
 pub async fn post_cache(pool: web::Data<Pool>, info: web::Path<Info>) -> impl Responder {
+    // TODO: check for ttl -> expire function, and get data from body
     let value: String = String::from("bar");
     let mut conn = pool.get().await.unwrap();
     let s: String = conn.set(info.key.to_string(), value).await.unwrap();
@@ -22,8 +24,8 @@ pub async fn post_cache(pool: web::Data<Pool>, info: web::Path<Info>) -> impl Re
 
 #[delete("/cache/{key}")]
 pub async fn delete_cache(pool: web::Data<Pool>, info: web::Path<Info>) -> impl Responder {
+    // TODO: Response was of incompatible type: "Response type not string compatible." (response was int(1))'
     let mut conn = pool.get().await.unwrap();
     let s: String = conn.del(info.key.to_string()).await.unwrap();
     HttpResponse::Ok().body(s)
 }
-
