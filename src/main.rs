1285496582fe5b4c9ea62da9e857fbd912f42b9b
Geoffrey Brunet <geoffrey.brunet@icloud.com>
2021-12-03 15:50:35 +0100

    route post with ttl works

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use actix_web::{web, App, HttpServer};
 use env_logger::Env;
 use mobc::Pool;
 use mobc_redis::{redis, RedisConnectionManager};
-use routes::cache::{delete_cache, get_cache, post_cache};
+use routes::cache::{delete_cache, get_cache, post_cache, post_cache_with_ttl};
 use routes::health_check::health_check;
 
 #[actix_web::main]
@@ -24,6 +24,7 @@ async fn main() -> std::io::Result<()> {
             .route("/health_check", web::get().to(health_check))
             .service(get_cache)
             .service(post_cache)
+            .service(post_cache_with_ttl)
             .service(delete_cache)
     })
     .bind(("127.0.0.1", 8080))?
--- src/routes/cache.rs
+++ src/routes/cache.rs
@@ -1,5 +1,5 @@
 use crate::structs::Info;
-use actix_web::{delete, get, post, web, HttpResponse, Responder, web::Bytes};
+use actix_web::{delete, get, post, web, web::Bytes, HttpResponse, Responder};
 use mobc_redis::redis::AsyncCommands;
 use mobc_redis::RedisConnectionManager;
 
@@ -14,14 +14,30 @@ pub async fn get_cache(pool: web::Data<Pool>, info: web::Path<Info>) -> impl Res
 }
 
 #[post("/cache/{key}")]
-pub async fn post_cache(pool: web::Data<Pool>, info: web::Path<Info>, bytes: Bytes) -> impl Responder {
-    // TODO: check for ttl -> expire function
+pub async fn post_cache(
+    pool: web::Data<Pool>,
+    info: web::Path<Info>,
+    bytes: Bytes,
+) -> impl Responder {
     let from_body: String = String::from_utf8(bytes.to_vec()).unwrap();
-    let value: String = String::from(from_body);
     let mut conn = pool.get().await.unwrap();
-    let s: String = conn.set(info.key.to_string(), value).await.unwrap();
+    let s: String = conn.set(info.key.to_string(), from_body).await.unwrap();
     HttpResponse::Ok().body(s)
 }
+
+#[post("/cache/{key}/{ttl_value}")]
+pub async fn post_cache_with_ttl(pool: web::Data<Pool>,
+                                 info: web::Path<Info>,
+                                 ttl_value: web::Path<Option<usize>>,
+                                 bytes: Bytes,) -> impl Responder {
+    let from_body: String = String::from_utf8(bytes.to_vec()).unwrap();
+    let mut conn = pool.get().await.unwrap();
+    let s: String = conn.set(info.key.to_string(), from_body).await.unwrap();
+    let selected_key: String = conn.get(info.key.to_string()).await.unwrap();
+    let s2: String = conn.expire(selected_key, ttl_value.unwrap()).await.unwrap();
+    HttpResponse::Ok().body(format!("Key {} have {} ttl.", s, s2))
+}
+
 
 #[delete("/cache/{key}")]
 pub async fn delete_cache(pool: web::Data<Pool>, info: web::Path<Info>) -> impl Responder {
