7da07783593b81fa654100cfb144b2f8649e9535
Geoffrey Brunet <geoffrey.brunet@icloud.com>
2021-12-03 15:51:06 +0100

    fmt

--- src/routes/cache.rs
+++ src/routes/cache.rs
@@ -26,10 +26,12 @@ pub async fn post_cache(
 }
 
 #[post("/cache/{key}/{ttl_value}")]
-pub async fn post_cache_with_ttl(pool: web::Data<Pool>,
-                                 info: web::Path<Info>,
-                                 ttl_value: web::Path<Option<usize>>,
-                                 bytes: Bytes,) -> impl Responder {
+pub async fn post_cache_with_ttl(
+    pool: web::Data<Pool>,
+    info: web::Path<Info>,
+    ttl_value: web::Path<Option<usize>>,
+    bytes: Bytes,
+) -> impl Responder {
     let from_body: String = String::from_utf8(bytes.to_vec()).unwrap();
     let mut conn = pool.get().await.unwrap();
     let s: String = conn.set(info.key.to_string(), from_body).await.unwrap();
@@ -37,7 +39,6 @@ pub async fn post_cache_with_ttl(pool: web::Data<Pool>,
     let s2: String = conn.expire(selected_key, ttl_value.unwrap()).await.unwrap();
     HttpResponse::Ok().body(format!("Key {} have {} ttl.", s, s2))
 }
-
 
 #[delete("/cache/{key}")]
 pub async fn delete_cache(pool: web::Data<Pool>, info: web::Path<Info>) -> impl Responder {
